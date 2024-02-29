3f447f6bd368bff3e923197e240a572484f8cd3f
Vedanti <vedanti,gavali@gmail.com>
2024-02-29 17:38:08 +0530

    All my files added

---
+++ Task1/index.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var list = document.getElementById("list");
+    var menuToggle = document.getElementById("menu-toggle");
+    list.classList.toggle("show");
+    menuToggle.classList.toggle("open");
+}
